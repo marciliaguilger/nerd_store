ef95e9019b70c1837d63ec4a1706f841f113978b
Marcilia <guilgerm@gmail.com>
2020-10-09 22:07:01 -0300

    Setup do event sourcing; criação da camada building blocks para artefatos da arquitetura

---
+++ src/EventSourcing/IEventStoreService.cs
@@ -0,0 +1,10 @@
+﻿using EventStore.ClientAPI;
+
+namespace EventSourcing
+{
+    //esta classe servirá como meio de comunicação com o bd Event Store
+    public interface IEventStoreService
+    {
+        IEventStoreConnection GetConnection();
+    }
+}
--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using EventSourcing;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using NerdStore.Catalogo.Application.Services;
 using NerdStore.Catalogo.Data;
@@ -31,6 +32,9 @@ namespace NerdStore.WebApp.MVC.Setup
 
             //Notifications
             services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+
+            //Event sourcing
+            services.AddSingleton<IEventStoreService, EventStoreService>(); //vai ficar valida para toda a aplicação enquanto a mesma estiver no ar
 
             //Catalogo
             // foi feito aqui pq a responsabilidade de resolver as dependencias fica fora da infra
